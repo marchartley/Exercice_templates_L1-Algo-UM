1fdb2a952a88b7392a0440096a3a34872d286fd3
kemar74 <marc.hartley@hotmail.com>
2021-10-04 17:24:36 +0200

    Template for first TP algo

---
+++ TP1_template.py
@@ -0,0 +1,46 @@
+# Exercice 1:
+
+def binome(n, p):
+    pass
+
+def binome_efficace(n, p):
+    pass
+
+# Exercice 2:
+def nbChifDec(n):
+    pass
+
+def chifRang(n, k):
+    pass
+
+def somChif(n):
+    pass
+
+def racineNumerique(n):
+    pass
+
+def invChif(n, nombre_inverse = 0):
+    pass
+
+# Exercice 3:
+def syracus(u):
+    pass
+
+def print_syracus(u):
+    pass
+
+def count_syracus(u):
+    pass
+
+def syracus_with_while():
+    pass
+
+# Exercice 4:
+def rechercheSequentielle(arr, n, x):
+    pass
+
+def rechercheDico(arr, debut, fin, x):
+    pass
+
+def rechercheTrico(arr, l, r, x):
+    pass
